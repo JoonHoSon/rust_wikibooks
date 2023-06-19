38a93c5be22e7d6157859a0bb9a0826fca2707aa
JoonHo Son <joonho.son@me.com>
2023-06-19 10:42:28 +0900

    feat : check3 추가

---
+++ check3/src/main.rs
@@ -0,0 +1,19 @@
+const A: &str = "A";
+
+fn main() {
+    for i in 1..50 {
+        if i % 3 == 0 || i % 10 == 3 {
+            println!("{}", A);
+
+            continue;
+        }
+
+        if i >= 30 && i <= 39 {
+            println!("{}", A);
+
+            continue;
+        }
+
+        println!("{}", i);
+    }
+}
