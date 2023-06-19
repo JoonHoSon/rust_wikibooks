da7326e27090a072a22399b5137f6e16389b3801
JoonHo Son <joonho.son@me.com>
2023-06-19 10:36:28 +0900

    feat : FizzBuzz 추가

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,17 @@
+const FIZZ: &str = "Fizz";
+const BUZZ: &str = "Buzz";
+const FIZZ_BUZZ: &str = "FizzBuzz";
+
+fn main() {
+    for i in 1..101 {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("{}", FIZZ_BUZZ);
+        } else if i % 3 == 0 {
+            println!("{}", FIZZ);
+        } else if i % 5 == 0 {
+            println!("{}", BUZZ);
+        } else {
+            println!("{}", i);
+        }
+    }
+}
