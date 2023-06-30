9c78d59790fffc35d6a5ba32ef23a0f472565385
JoonHo Son <joonho.son@me.com>
2023-06-30 09:45:21 +0900

    refactor: fizzbuss_file 예외 처리 추가

--- fizzbuzz_file/src/main.rs
+++ fizzbuzz_file/src/main.rs
@@ -1,11 +1,20 @@
 use std::fs::{self, File};
 use std::io::{BufWriter, Write};
+use std::process::exit;
 
 fn main() {
     let filename = "fizzbuzz_file_result.txt";
 
     {
-        let fp = File::create(filename).unwrap();
+        let fp: File = match File::create(filename) {
+            Ok(v) => v,
+            Err(e) => {
+                println!("{:?}", e.to_string());
+
+                exit(1);
+            }
+        };
+
         let mut writer: BufWriter<File> = BufWriter::new(fp);
 
         for i in 1..=100 {
@@ -21,10 +30,24 @@ fn main() {
 
             let bytes: &[u8] = line.as_bytes();
 
-            writer.write(bytes).unwrap();
+            match writer.write(bytes) {
+                Ok(v) => v,
+                Err(e) => {
+                    println!("{:?}", e.to_string());
+
+                    exit(1);
+                }
+            };
         }
     }
 
-    let s = fs::read_to_string(filename).unwrap();
+    let s: String = match fs::read_to_string(filename) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{:?}", e);
+
+            exit(1);
+        }
+    };
     println!("{}", s);
 }
