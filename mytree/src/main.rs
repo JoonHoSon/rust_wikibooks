e61b1a1eb76eea37caab49334909ca35a87abd64
JoonHo Son <joonho.son@me.com>
2023-07-03 10:07:58 +0900

    feat: mytree 추가

---
+++ mytree/src/main.rs
@@ -0,0 +1,56 @@
+use std::{env, path, process::exit};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut target_dir = ".";
+
+    if args.len() > 1 {
+        target_dir = &args[1];
+    }
+
+    let target = path::PathBuf::from(target_dir);
+    println!("{}", target_dir);
+
+    tree(&target, 0);
+}
+
+fn tree(target: &path::PathBuf, level: isize) {
+    let files = match target.read_dir() {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{:?}", e);
+            exit(1);
+        }
+    };
+
+    for f in files {
+        let path: path::PathBuf = match f {
+            Ok(v) => v.path(),
+            Err(e) => {
+                println!("{:?}", e);
+                exit(1);
+            }
+        };
+
+        for _ in 1..=level {
+            print!("|    ");
+        }
+
+        let filename = match path.file_name() {
+            Some(v) => v.to_string_lossy(),
+            None => {
+                println!("파일명을 확인할 수 없습니다.");
+                exit(1);
+            }
+        };
+
+        if path.is_dir() {
+            println!("|---- <{}>", filename);
+            tree(&path, level + 1);
+
+            continue;
+        }
+
+        println!("|---- {}", filename);
+    }
+}
