c5975931965690cb2afa8c12b087dad16149b2c5
JoonHo Son <joonho.son@me.com>
2023-07-03 11:18:59 +0900

    feat: owner_func_mut 추가

---
+++ owner_func_mut/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut msg: String = String::from("건강한 신체에 건강한 정신이 깃든다.");
+
+    println!("{}", msg);
+    add_quote(&mut msg);
+    println!("{}", msg);
+}
+
+fn add_quote(msg: &mut String) {
+    msg.insert(0, '"');
+    msg.push('"');
+}
