406f92b58553293be65fa19179808958e8e2eada
JoonHo Son <joonho.son@me.com>
2023-07-04 10:33:43 +0900

    feat: tuple_goods_struct 추가

---
+++ tuple_goods_struct/src/main.rs
@@ -0,0 +1,27 @@
+struct Item(String, i64);
+
+fn main() {
+    let banana: Item = Item("바나나".to_string(), 300);
+    let apple: Item = Item("사과".to_string(), 200);
+    let mango: Item = Item("망고".to_string(), 500);
+    let items: Vec<Item> = vec![banana, apple, mango];
+    let total: i64= print_and_sum_items(&items);
+
+    println!("합계는 {}원 입니다.", total);
+}
+
+fn print_tuple(item: &Item) {
+    println!("{}를 {}원에 구입", item.0, item.1);
+}
+
+fn print_and_sum_items(items: &Vec<Item>) -> i64 {
+    let mut total = 0;
+
+    for i in items {
+        print_tuple(&i);
+
+        total += i.1;
+    }
+
+    total
+}
