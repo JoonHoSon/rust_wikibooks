e7e2bdfd03f2f821c2e8a59103dc94d16b421e95
JoonHo Son <joonho.son@me.com>
2023-07-04 10:47:24 +0900

    feat: struct_wh 추가

---
+++ struct_wh/src/main.rs
@@ -0,0 +1,22 @@
+struct Body {
+    weight: f64,
+    height: f64,
+}
+
+fn main() {
+    let hong = Body {
+        weight:80.0,
+        height: 165.0,
+    };
+    let lim = Body {
+        weight: 65.0,
+        height: 170.0
+    };
+
+    println!("홍길동 = {:.1}", calc_bmi(&hong));
+    println!("임꺽정 = {:.1}", calc_bmi(&lim));
+}
+
+fn calc_bmi(body: &Body) -> f64 {
+    body.weight / (body.height / 100.0).powf(2.0)
+}
