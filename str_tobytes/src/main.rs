ba61f538bd1cabca6fafde88bf473f60216b2e4c
JoonHo Son <joonho.son@me.com>
2023-07-04 11:36:43 +0900

    feat: str_tobytes 추가

---
+++ str_tobytes/src/main.rs
@@ -0,0 +1,15 @@
+use std::print;
+
+fn main() {
+    let pr: &str= "구슬이 서 말이라도 꿰어야 보배";
+
+    for (idx, c) in pr.bytes().enumerate() {
+        print!("{:2x}", c);
+
+        if idx < pr.bytes().len() - 1 {
+            print!(" ");
+        }
+    }
+
+    println!("\n바이트 = {}Bytes", pr.len());
+}
