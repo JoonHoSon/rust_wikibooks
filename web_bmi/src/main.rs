0f71b62e069876a186c0e002500c0cfb2f4d28bd
JoonHo Son <joonho.son@me.com>
2023-07-20 13:39:24 +0900

    fix: debug/release에 따라 상수 분기 테스트

--- web_bmi/src/main.rs
+++ web_bmi/src/main.rs
@@ -1,6 +1,9 @@
-use std::sync::Mutex;
+extern crate actix_web;
+extern crate lazy_static;
+extern crate tera;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, post, Responder, web};
+use lazy_static::lazy_static;
 use serde::Deserialize;
 use tera::{Context, Tera};
 
@@ -35,11 +38,40 @@ pub struct FormBMI {
 //         .body(format!("<h3> BMI = {:.1}, 비만율 = {:.0}%</h3>", bmi, per)));
 // }
 
+#[cfg(debug_assertions)]
+const ROOT_PATH: &str = env!("CARGO_MANIFEST_DIR");
+
+#[cfg(not(debug_assertions))]
+const ROOT_PATH: &str = "/Users/joonho/Desktop/tera_root";
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        // let mut tera: Tera = match Tera::new(concat!(ROOT_PATH, "/templates/**/*.html")) {
+        //     Ok(v) => v,
+        //     Err(e) => {
+        //         println!("Template parsing error : {:?}", e);
+        //         std::process::exit(1);
+        //     },
+        // };
+        let mut tera: Tera = match Tera::new(format!("{}{}", ROOT_PATH, "/templates/**/*.html").as_str()) {
+            Ok(v) => v,
+            Err(e) => {
+                println!("Template parsing error : {:?}", e);
+                std::process::exit(1);
+            },
+        };
+
+        tera.autoescape_on(vec!["html"]);
+        tera.full_reload().unwrap();
+
+        return tera;
+    };
+}
+
 #[get("/")]
-async fn index(tera: web::Data<Mutex<Tera>>) -> impl Responder {
-    let tera = tera.lock().unwrap();
+async fn index(_: HttpRequest) -> impl Responder {
     let context: Context = Context::new();
-    let render = tera.render("index.html", &context).unwrap();
+    let render = TEMPLATES.render("index.html", &context).unwrap();
 
     return HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
@@ -47,17 +79,16 @@ async fn index(tera: web::Data<Mutex<Tera>>) -> impl Responder {
 }
 
 #[post("/calc")]
-async fn calc(tera: web::Data<Mutex<Tera>>, q: web::Form<FormBMI>) -> impl Responder {
+async fn calc(q: web::Form<FormBMI>) -> impl Responder {
     let h = q.height / 100.0;
     let bmi = q.weight / (h * h);
     let per = (bmi / 22.0) * 100.0;
-    let tera = tera.lock().unwrap();
     let mut context: Context = Context::new();
 
     context.insert("bmi", format!("{:.1}", bmi).as_str());
     context.insert("per", format!("{:.0}", per).as_str());
 
-    let render = tera.render("result.html", &context).unwrap();
+    let render = TEMPLATES.render("result.html", &context).unwrap();
 
     return HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
@@ -66,18 +97,18 @@ async fn calc(tera: web::Data<Mutex<Tera>>, q: web::Form<FormBMI>) -> impl Respo
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!(
-        "tera path : {}",
-        concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*.html")
-    );
-
-    let tera = web::Data::new(Mutex::new(
-        Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*.html")).unwrap(),
-    ));
+    // println!(
+    //     "tera path : {}",
+    //     concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*.html")
+    // );
+    //
+    // let tera = web::Data::new(Mutex::new(
+    //     Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*.html")).unwrap(),
+    // ));
 
     HttpServer::new(move || {
         App::new()
-            .app_data(tera.clone())
+            // .app_data(tera.clone())
             .service(index)
             .service(calc)
     })
