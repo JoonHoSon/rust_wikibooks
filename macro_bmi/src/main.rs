a7511bcd5aab1efc3858895542e437fae8f72ed5
JoonHo Son <joonho.son@me.com>
2023-07-22 09:11:27 +0900

    feat: macro_bmi 추가

---
+++ macro_bmi/src/main.rs
@@ -0,0 +1,30 @@
+#[macro_export]
+macro_rules! bmi_select {
+    ($bmi:expr, $($label:expr => $range:expr);+) => {{
+        let mut result = "계산불가";
+        $(
+            if $range.start <= $bmi && $bmi < $range.end {
+                result = $label;
+            }
+        )+
+
+        result
+    }};
+}
+
+fn main() {
+    let h: f32 = 158.0;
+    let w: f32 = 63.0;
+    let bmi = w / (h / 100.0).powf(2.0);
+    let label = bmi_select![
+        bmi,
+        "저체중" => 0.0..18.5;
+        "정상" => 18.5..23.0;
+        "비만 전단계" => 23.0..25.0;
+        "1단계 비만" => 25.0..30.0;
+        "2단계 비만" => 30.0..35.0;
+        "3단계 비만" => 35.0..99.0
+    ];
+
+    println!("결과 : {}", label);
+}
